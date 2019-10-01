78234726b1f8096995cb32ee32bfd1538c8f0232
ibrahim <ichibsah@gmail.com>
2019-10-01 10:50:01 +0200

    01/10/2019 10:50:01,45

--- github-push.bat
+++ github-push.bat
@@ -1,7 +1,7 @@
 git add .
 git commit -m "%date% %time%"
-REM git remote add origin https://github.com/ichibsah/AddOns.git
-REM git push -u origin master
+git remote add origin https://github.com/ichibsah/AddOns.git
+git push -u origin master
 git push https://github.com/ichibsah/AddOns.git
 
 pause
