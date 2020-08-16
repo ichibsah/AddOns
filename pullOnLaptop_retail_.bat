29ec46d2bb136a71ae3e435cd3bd25b3d4ff8688
Ibrahim Sawadogo <ibrahim@skyline.lan>
2020-08-16 12:06:34 +0200

    16/08/2020 12:06:34,29

---
+++ pullOnLaptop_retail_.bat
@@ -0,0 +1,9 @@
+rem https://github.com/ichibsah/AddOnsClassic/archive/master.zip
+
+c:
+cd "\Program Files (x86)\World of Warcraft\_retail_\Interface\AddOns"
+git init "c:\Program Files (x86)\World of Warcraft\_retail_\Interface\AddOns"
+git remote add origin  https://github.com/ichibsah/AddOns.git
+git pull https://github.com/ichibsah/AddOns.git
+
+pause
